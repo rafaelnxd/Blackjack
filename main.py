6d20c46f74a05426ad8012149084bb7c44eae49c
rafaelnxd <rafaelnovaes46@outlook.com>
2023-10-31 12:36:26 -0300

    Deck Class

--- main.py
+++ main.py
@@ -1,37 +1,45 @@
 import random
+class Deck:
+    def __init__(self):
+        self.cards = []
+        suits = ["spades", "clubs", "hearts", "diamonds"]
+        ranks = [
+                        {"rank": "A", "value": 11},
+                        {"rank": "2", "value": 2},
+                        {"rank": "3", "value": 3},
+                        {"rank": "4", "value": 4},
+                        {"rank": "5", "value": 5},
+                        {"rank": "6", "value": 6},
+                        {"rank": "7", "value": 7},
+                        {"rank": "8", "value": 8},
+                        {"rank": "9", "value": 9},
+                        {"rank": "10", "value": 10},
+                        {"rank": "J", "value": 10},
+                        {"rank": "Q", "value": 10},
+                        {"rank": "K", "value": 10},
+                    ]
 
-cards = []
-suits = ["spades", "clubs", "hearts", "diamonds"]
-ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+        for suit in suits:
+            for rank in ranks:
+                self.cards.append([suit, rank])
 
-for suit in suits:
-    for rank in ranks:
-        cards.append([suit, rank])
+    def shuffle(self):
+        if len(self.cards) > 1:
+            random.shuffle(self.cards)
 
-def shuffle():
-    random.shuffle(cards)
+    def deal(self, number):
+        cards_dealt = []
+        for x in range(number):
+            if len(self.cards) > 0:
+                card = self.cards.pop()
+                cards_dealt.append(card)
+        return cards_dealt
 
-def deal(number):
-    cards_dealt = []
-    for x in range(number):
-        card = cards.pop()
-        cards_dealt.append(card)
-    return cards_dealt
-
-shuffle()
-cards_dealt = deal(2)
-card = cards_dealt[0]
-rank = card[1]
-
-if rank == "A":
-    value = 11
-elif rank == "J" or rank == "Q" or rank == "K":
-    value = 10
-else:
-    value = rank
-
-print(rank, value)
-
-
+    
+deck1 = Deck()
+deck2 = Deck()
+deck2.shuffle()
+print(deck1.cards)
+print(deck2.cards)
 
 
